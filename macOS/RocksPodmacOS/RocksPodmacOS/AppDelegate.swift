cb2dbd340309bade5a495fffa8e1ec3685dbf100
Rob Phillips <rob@getkeepsafe.com>
2018-05-24 10:28:05 -0400

    [macOS] ObjectiveRocks integration example

---
+++ macOS/RocksPodmacOS/RocksPodmacOS/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  RocksPodmacOS
+//
+//  Created by Rob Phillips on 5/24/18.
+//  Copyright © 2018 ObjectiveRocks. All rights reserved.
+//
+
+import Cocoa
+
+// See if it builds with this import
+import ObjectiveRocks
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+}
+
