265375c4a002f0aa6f87c5ada12cfbd748a68ae1
Rob Phillips <rob@getkeepsafe.com>
2018-05-24 10:22:42 -0400

    [iOS] ObjectiveRocks integration example

---
+++ iOS/RocksPodiOS/RocksPodiOS/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  RocksPodiOS
+//
+//  Created by Rob Phillips on 5/24/18.
+//  Copyright © 2018 ObjectiveRocks. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ iOS/RocksPodiOS/RocksPodiOS/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  RocksPodiOS
+//
+//  Created by Rob Phillips on 5/24/18.
+//  Copyright © 2018 ObjectiveRocks. All rights reserved.
+//
+
+import UIKit
+
+// See if it builds with this import
+import ObjectiveRocks
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
